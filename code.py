be02679a9501c33e17c19c04139546c17fdc9c5f
Evan Sharp <ego@evansharp.ca>
2018-02-05 12:51:22 -0800

    Create code.py

---
+++ code.py
@@ -0,0 +1,10 @@
+# This function will print a greeting
+
+def greetingPrinter():
+  print("-------------------------")
+  print("|        Hello!         |")
+  print("-------------------------")
+
+
+# use it by calling the function by name
+greetingPrinter()
